3bde87e40c1a0d533cb6d07c97d5d4be297b87d9
piezancu <162472160+piezancu@users.noreply.github.com>
2024-07-02 12:09:16 +0200

    Update DAO.py

--- database/DAO.py
+++ database/DAO.py
@@ -2,5 +2,45 @@ from database.DB_connect import DBConnect
 
 
 class DAO():
-    def __init__(self):
-        pass
+    @staticmethod
+    def getChromosomes():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor()
+        query = """ select distinct Chromosome 
+                    from genes where Chromosome != 0  """
+
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(row[0])
+
+        cursor.close()
+        conn.close()
+        return result
+
+
+    @staticmethod
+    def getEdgeTo(node1, node2):
+        conn = DBConnect.get_connection()
+
+        result = [False, 0]
+
+        cursor = conn.cursor()
+        query = """select distinct g1.Chromosome, g2.Chromosome, i.GeneID1, i.GeneID2 , i.Expression_Corr as ec
+                from genes g1, genes g2, interactions i  
+                where g1.Chromosome = %s and g2.Chromosome = %s and
+                g2.GeneID = i.GeneID2 and g1.GeneID = i.GeneID1 """
+
+        cursor.execute(query, (node1, node2))
+
+        for row in cursor:
+            result[0] = True
+            result[1] += row[4]
+
+        cursor.close()
+        conn.close()
+
+        return result
